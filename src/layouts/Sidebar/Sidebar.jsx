498120da9eff87f4181c0d700515c2ad1993ee25
UlisesRodriguezPerez <uli.rp1999@gmail.com>
2023-09-23 22:23:39 -0600

    Sidebar responsive

--- src/layouts/Sidebar/Sidebar.jsx
+++ src/layouts/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import headerimage from './../../assets/images/layouts/header-tec.png';
@@ -7,6 +7,8 @@ import { useNavigate } from 'react-router-dom';
 
 export const Sidebar = ({ onLogout, options = [] }) => {
 
+    const [isVisible, setIsVisible] = useState(true);
+
     const navigate = useNavigate();
 
     const handleOptionClick = (path) => {
@@ -14,28 +16,62 @@ export const Sidebar = ({ onLogout, options = [] }) => {
     };
 
     const goToHome = () => {
-        navigate('/admin-dashboard');  // Navigate to Home view
+        const user = JSON.parse(localStorage.getItem('user')) || {};
+        const roles = user.roles || [];
+        const isAdmin = roles.some(role => role.name === 'admin');
+
+        if (isAdmin) {
+            navigate('/admin-dashboard');
+        } else {
+            navigate('/user-dashboard');
+        }
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 920) {
+                setIsVisible(false);
+            } else {
+                setIsVisible(true);
+            }
+        };
+
+        // Ejecutar una vez al montar para establecer el estado inicial
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
 
     return (
-        <aside className="sidebar">
-            <div className="sidebar-top" onClick={goToHome}>
-                <img src={headerimage} alt="Logo" className="sidebar-logo" />
-            </div>
-            <div className="sidebar-options">
-                {options.map((option, index) => (
-                    <button key={index} className="option" onClick={() => handleOptionClick(option.path)}>
-                        {option.title}
-                    </button>
-                ))}
-            </div>
-            <div className="sidebar-bottom">
-                <button className="action-button logout-action" onClick={onLogout}>
-                    <FontAwesomeIcon icon={faSignOutAlt} aria-hidden="true" />
-                    Salir
-                </button>
-            </div>
-        </aside>
+        <div>
+            {isVisible ? (
+                <aside className={`sidebar ${isVisible ? 'visible' : ''}`}>
+                    <div className="sidebar-top" onClick={goToHome}>
+                        <img src={headerimage} alt="Logo" className="sidebar-logo" />
+                    </div>
+                    <div className="sidebar-options">
+                        {options.map((option, index) => (
+                            <button key={index} className="option" onClick={() => handleOptionClick(option.path)}>
+                                {option.title}
+                            </button>
+                        ))}
+                    </div>
+                    <div className="sidebar-bottom">
+                        <button className="action-button logout-action" onClick={onLogout}>
+                            <FontAwesomeIcon icon={faSignOutAlt} aria-hidden="true" />
+                            Salir
+                        </button>
+                    </div>
+                </aside>
+            ) : null}
+            <button className={`toggle-sidebar-btn ${isVisible ? 'beside-logout' : ''}`} onClick={() => setIsVisible(!isVisible)}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </button>
+        </div>
     );
 };
