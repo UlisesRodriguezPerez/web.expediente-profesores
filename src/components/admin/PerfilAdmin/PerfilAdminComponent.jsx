a7dddab6f931a193879afad4ad0bb04d4f38dd79
DavidEspinozaNemo <david1998es16@gmail.com>
2023-10-18 19:34:50 -0600

    variables necesarias

--- src/components/admin/PerfilAdmin/PerfilAdminComponent.jsx
+++ src/components/admin/PerfilAdmin/PerfilAdminComponent.jsx
@@ -9,16 +9,40 @@ import { useContext } from 'react';
 import { NotificationContext } from '../../../contexts/NotificationContext/NotificationContext';
 
 export const PerfilAdminComponent = () => {
-    const [name, setName] = useState('');
-    const [lastname1, setLastname1]  = useState('');
-    const [lastname2, setLastname2]  = useState('');
-    const [phone, setPhone]  = useState('');
-    const [email, setEmail]  = useState('');
+    // id del usuario que abre la pagina
+    const [id, setUserPage] = useState(0);
+    // ids del collaborador, los datos que buscamos
+    //  const responseData = await dataService.readData(ROUTES.COLLABORATORS, id);
+    const [id_user, setUserId] = useState(0);
+    const [id_position, setUserPosition] = useState(0);
+    const [id_category, setUserCategory] = useState(0);
+    const [id_appointment, setUserAppointment] = useState(0);
+    const [id_degree, setUserDegree] = useState(0);
+    const [id_campus, setUserCampus] = useState(0);
+    // aqui van la busqueda de los valores strings
+    //  const responseData = await dataService.readData(ROUTES.USERS, id_user);
+    const [name_users, setNameUser] = useState('');
+    const [last_name_users, setLastnameUser]  = useState('');
+    const [second_last_name_users, setSecondlastUser]  = useState('');
+    const [phone_users, setPhoneUser]  = useState('');
+    const [email_users, setEmailUser]  = useState('');
+    //  const responseData = await dataService.readData(ROUTES.TEC_CATEGORIES, id_category);
+    const [name_tec_category, setTecCategory] = useState('');
+    //  const responseData = await dataService.readData(ROUTES.APPOINTMENT_TYPES, id_appointment);
+    const [name_appointment, setNameAppointment] = useState('');
+    //  const responseData = await dataService.readData(ROUTES.ACADEMIC_DEGREES, id_degree);
+    const [name_academic_degrees, setAcademicDegrees] = useState('');
+    //  const responseData = await dataService.readData(ROUTES.CAMPUSES, id_campus);
+    const [name_campus, setNameCampus] = useState('');
+    //  const responseData = await dataService.readData(ROUTES.POSITIONS, id_position);
+    const [name_position, setNamePosition] = useState('');
 
+    // ------------------- fin de la variables ---------------------------
+    
     const { showNotification } = useContext(NotificationContext);
 
     const handleSubmit = async () => {
-        console.log(name, lastname1, lastname2, phone, email);
+        //console.log(name, lastname1, lastname2, phone, email);
     };
 
     return (
