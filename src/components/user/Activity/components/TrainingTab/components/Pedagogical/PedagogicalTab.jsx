f72cbbe7f84edf7c30b177e45813ddfb77bf3de6
GabyGV <56004250+GabyGV@users.noreply.github.com>
2023-11-04 22:52:54 -0600

    Update PedagogicalTab.jsx

--- src/components/user/Activity/components/TrainingTab/components/Pedagogical/PedagogicalTab.jsx
+++ src/components/user/Activity/components/TrainingTab/components/Pedagogical/PedagogicalTab.jsx
@@ -1,14 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { NotificationContext } from '../../../../../../../contexts/NotificationContext/NotificationContext';
 import './PedagogicalTab.css';
+import dataService from '../../../../../../../services/dataService.js'
+import ROUTES from '../../../../../../../enums/routes'; //posiblemente arreglar
 
 export const PedagogicalTab = () => {
 
   const [course, setCourse] = useState(''); 
   const [institution, setInstitution] = useState('');
+  const { showNotification } = useContext(NotificationContext);
   
-  const handleAddPedagogical = () => {
-   
+  const handleAddPedagogical = async () => {
     alert(`Curso: ${course}\nInstitución: ${institution}`);
+    try{
+      if (!course || !institution) {
+        showNotification('error', 'Todos los campos son requeridos.');
+        return; // Not continue if any field is empty
+      }
+
+      const responseUserId = await dataService.readData(ROUTES.USERS, localStorage.getItem('userId'));
+      //Obtener periodo
+
+      await dataService.createData(`${ROUTES.ACTIVITIES}`, {
+        period_id:5,
+        creator_id: responseUserId,
+        involved_id: responseUserId,
+        name: course 
+      });
+
+      const responseActivityId = await dataService.readData(`${ROUTES.ACTIVITIES}?filter[creator_id]=${responseUserId},[name]=${course}&included=id`);
+
+      await dataService.createData(`${ROUTES.PEDAGOGICAL_TRAININGS}`, {
+        activity_id:responseActivityId.data.data[0].activity_id,
+        institution_name: institution
+      });
+
+      showNotification('success', 'Actividad asignada exitosamente');
+    }
+    catch (error) {
+      console.error('Error al guardar actividad pedagogica:', error);
+    }
+    
   };
 
   return (
